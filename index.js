dfdf149351144759aba1fe1af7dd069512061fee
Mat Gilbert <matgilbert@gmail.com>
2016-02-18 00:20:39 -0500

    refactored code blob into more discrete functions

--- index.js
+++ index.js
@@ -2,63 +2,115 @@
 var shapes = require('svg-path-shapes')
 var normalize = require('normalize-svg-path')
 var parse = require('parse-svg-path')
-//var svgb = require('svg-builder')
 var _ = require('lodash')
 var simplify = require('simplify-js')
+var cheerio = require('cheerio')
+var fs = require('fs');
 
+
+//Hardcoded filepath, to be replaced with command line arguments
 var path = __dirname + '/6.01.svg';
 
-var fs = require('fs');
-var cheerio = require('cheerio'),
-    $ = cheerio.load(fs.readFileSync(path));
+var svgPaths = parseSVGPaths(path)
+var seperatedPaths = seperatePaths(svgPaths);
+console.log(wrapSVG(seperatedPaths));
 
-var traces = _.reduce($('path'), function (items, path) {
-	items.push($(path).attr('d'))
-	return items
-}, [])
+//Takes in a multi-shape path and 
+function seperatePaths(paths){
 
+	var pathElements = [];
 
-var strOutput = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>';	
-	strOutput += '<svg id="workspace" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 531 360" version="1.1">';
+	_(paths).forEach(function(layer){
 
-_(traces).forEach(function(layer){
+		_(shapes(layer)).forEach(function(shape){
 
-	_(shapes(layer)).forEach(function(shape){
+			var pathPoints = parse(shape)
 
-		var pathPoints = parse(shape)
+			var pathPoints = _.reduce(parse(shape), function (items, points) {
+				var values = points.slice(1)
+				items.push({ x: values[0], y: values[1]})
+				return items
+			}, [])		
 
-		var pathPoints = _.reduce(parse(shape), function (items, points) {
-			
-			var values = points.slice(1)
-			items.push({ x: values[0], y: values[1]})
-			
-			return items
-		}, [])		
+			pathElements.push(wrapPath(pathPoints))
 
-		var simplifiedPath = simplify(pathPoints,.1,true)
-
-		//var bezierPath = normalize(pathPoints)
-		var bezierPath = normalize(simplifiedPath)		
-
-		strOutput += '<path d="'
-		strOutput += 'M' + bezierPath[0].x + ',' + bezierPath[0].y + ' '
-
-		_(bezierPath.slice(1)).forEach(function(element){
-			strOutput += ' L ' + element.x + ',' + element.y + ' '
 		})
 
-		strOutput += '" stroke="#000000" fill="none"/>';
+	})	
 
-	});
-});
+	return pathElements
+}
 
-strOutput += '</svg>';
+function wrapPath(pathData){
 
-function outputSVG(paths){
+	var strOutput = '<path d="'
 
+	_(pathData).forEach(function(element){
+		strOutput += ' M ' + element.x + ',' + element.y + ' '
+	})
+
+	strOutput += '" stroke="#000000" fill="none"/>';
+
+	return strOutput
 
 }
 
-console.log(strOutput)
+function wrapSVG(paths){
 
+	var strOutput = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>';	
+	strOutput += '<svg id="workspace" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 531 360" version="1.1">';
 
+	console.log('paths!',paths)
+
+		_(paths).forEach(function(element){
+			
+			console.log(element)
+			strOutput += element
+		})
+
+	strOutput += '</svg>';
+
+	return strOutput;
+
+}
+
+//Takes in an SVG file, returns an array of paths elements
+function parseSVGPaths(filepath){
+
+	$ = cheerio.load(fs.readFileSync(path));
+	var paths = _.reduce($('path'), function (items, path) {
+		items.push($(path).attr('d'))
+		return items
+	}, [])
+
+	return paths
+}
+
+//Original Sketch / Mockup, 
+//Includes non-essential simplification and bezier conversion
+
+// _(traces).forEach(function(layer){
+// 	_(shapes(layer)).forEach(function(shape){
+
+// 		var pathPoints = parse(shape)
+
+// 		var pathPoints = _.reduce(parse(shape), function (items, points) {
+// 			var values = points.slice(1)
+// 			items.push({ x: values[0], y: values[1]})
+// 			return items
+// 		}, [])		
+
+// 		var simplifiedPath = simplify(pathPoints,.1,true)
+// 		var bezierPath = normalize(simplifiedPath)		
+
+// 		strOutput += '<path d="'
+// 		strOutput += 'M' + bezierPath[0].x + ',' + bezierPath[0].y + ' '
+
+// 		_(bezierPath.slice(1)).forEach(function(element){
+// 			strOutput += ' L ' + element.x + ',' + element.y + ' '
+// 		})
+
+// 		strOutput += '" stroke="#000000" fill="none"/>';
+
+// 	})
+// })
